852223cec77f788de806ef4e861121ce3587db8b
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2026-07-04 18:38:55 -0500

    commit n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { join } from 'path'; //propio de node.
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    PokemonModule,
   ],
 })
 export class AppModule {}
